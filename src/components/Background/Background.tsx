bddb53ad69c5db3f83fe92a0e731272a6eea3734
Ayoralol <121880251+Ayoralol@users.noreply.github.com>
2024-04-21 02:18:11 +1000

    fixed responsiveness issues (still not mobile friendly)

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -8,33 +8,8 @@ interface BackgroundProps {
 }
 
 const Background: React.FC<BackgroundProps> = ({children}) => {
-  const [loc, setLoc] = useState("");
-  const location = useLocation();
-
-  useEffect(() => {
-    const path = location.pathname;
-
-    switch (path) {
-      case "/":
-        setLoc("Home");
-        break;
-      case "/encode-existing":
-        setLoc("Encode");
-        break;
-      case "/decode":
-        setLoc("Decode");
-        break;
-      case "/how-to":
-        setLoc("How To");
-        break;
-      default:
-        setLoc("?????");
-    }
-  }, [location.pathname]);
-
   return (
     <div className={styles.cont}>
-      <p className={styles.loc}>{loc}</p>
       <div className={styles.lCont}>
         <img src={IMAGES.bg} className={styles.lCont__img} />
       </div>
